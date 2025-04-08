0ca41d94fb48228f88b377c2f00615cfafc7a054
Oliver Walczak <olly.walczak@gmx.de>
2025-04-08 21:41:20 +0200

    Add line coding properties according to Phil's Lab video

--- USB_DEVICE/App/usbd_cdc_if.c
+++ USB_DEVICE/App/usbd_cdc_if.c
@@ -94,7 +94,13 @@ uint8_t UserRxBufferFS[APP_RX_DATA_SIZE];
 uint8_t UserTxBufferFS[APP_TX_DATA_SIZE];
 
 /* USER CODE BEGIN PRIVATE_VARIABLES */
-
+// Add line coding according to https://youtu.be/kDaQO_x8auk?si=Egu2Jjes0wwT3R9h
+USBD_CDC_LineCodingTypeDef LineCoding = {
+  115200,                       /* baud rate */
+  0x00,                         /* stop bits - 1 */
+  0x00,                         /* parity - none */
+  0x08                          /* nb. of bits 8 */
+};
 /* USER CODE END PRIVATE_VARIABLES */
 
 /**
@@ -221,12 +227,21 @@ static int8_t CDC_Control_FS(uint8_t cmd, uint8_t* pbuf, uint16_t length)
   /* 6      | bDataBits  |   1   | Number Data bits (5, 6, 7, 8 or 16).          */
   /*******************************************************************************/
     case CDC_SET_LINE_CODING:
-
-    break;
+      LineCoding.bitrate = (uint32_t) (pbuf[0] | (pbuf[1] << 8) | (pbuf[2] << 16) | (pbuf[3] << 24));
+      LineCoding.format = pbuf[4];
+      LineCoding.paritytype = pbuf[5];
+      LineCoding.datatype = pbuf[6];
+      break;
 
     case CDC_GET_LINE_CODING:
-
-    break;
+      pbuf[0] = (uint8_t) (LineCoding.bitrate);
+      pbuf[1] = (uint8_t) (LineCoding.bitrate >> 8);
+      pbuf[2] = (uint8_t) (LineCoding.bitrate >> 16);
+      pbuf[3] = (uint8_t) (LineCoding.bitrate >> 24);
+      pbuf[4] = LineCoding.format;
+      pbuf[5] = LineCoding.paritytype;
+      pbuf[6] = LineCoding.datatype;
+      break;
 
     case CDC_SET_CONTROL_LINE_STATE:
 
